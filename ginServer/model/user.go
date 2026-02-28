d7bbbbee2d65117b3ddf4c07f6796c5ef0e9f018
cyh <317279541@qq.com>
2026-02-28 16:07:41 +0800

    feat: 添加微信相关字段到 User 模型

--- ginServer/model/user.go
+++ ginServer/model/user.go
@@ -6,7 +6,12 @@ import "time"
 type User struct {
 	ID           uint      `json:"id" gorm:"primaryKey;AUTO_INCREMENT"`
 	Username     string    `json:"username" gorm:"type:TEXT;uniqueIndex;not null"`
-	PasswordHash string    `json:"-" gorm:"type:TEXT;not null"` // 不返回给前端
+	PasswordHash string    `json:"-" gorm:"type:TEXT"`             // 不返回给前端
+	OpenID       string    `json:"-" gorm:"type:TEXT;uniqueIndex"` // 微信 openid
+	SessionKey   string    `json:"-" gorm:"type:TEXT"`             // 微信 session_key
+	Nickname     string    `json:"nickname" gorm:"type:TEXT"`      // 微信昵称
+	Avatar       string    `json:"avatar" gorm:"type:TEXT"`        // 微信头像
+	WechatID     string    `json:"-" gorm:"type:TEXT"`             // 微信统一ID
 	CreatedAt    time.Time `json:"created_at" gorm:"autoCreateTime"`
 	UpdatedAt    time.Time `json:"updated_at" gorm:"autoUpdateTime"`
 }
@@ -15,4 +20,3 @@ type User struct {
 func (User) TableName() string {
 	return "users"
 }
-
